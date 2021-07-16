b4d0a977a9948cd1ea87901759deefbbe3a3817b
Carlos Oliveira <oslcarlos@gmail.com>
2021-07-15 22:56:23 -0300

    Converçoes e Outro Tipos Numericos

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Execultar o projeto 4");
+
+            double salario;
+            salario = 1200.50;
+
+            // O int é um tipo de variavel que suporta valores ate 32 bits.
+            int salarioInteiro;
+            salarioInteiro = (int)salario;
+
+            Console.WriteLine(salarioInteiro);
+
+            // O long é uma variavel de 64 bits.
+            long idade = 140000000000000;
+            Console.WriteLine(idade);
+
+            // O short é uma variavel que suporta ate 16 bits.
+            short quantidadeProduto =15000;
+            Console.WriteLine(quantidadeProduto);
+           
+            float altura = 1.66f;
+            Console.WriteLine(altura);
+             
+
+            Console.ReadLine();
+        }
+    }
+}
